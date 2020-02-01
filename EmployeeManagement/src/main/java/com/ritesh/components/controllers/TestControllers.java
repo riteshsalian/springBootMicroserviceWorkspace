aae98858394839a654e038bc8670971fcb5ec032
Salian <riteshkumar.salian@capgemini.com>
2020-02-01 18:54:54 +0530

    Created a new springboot app

---
+++ EmployeeManagement/src/main/java/com/ritesh/components/controllers/TestControllers.java
@@ -0,0 +1,16 @@
+package com.ritesh.components.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestControllers {
+	
+	@RequestMapping("/")
+	public String checkStatus()
+	{
+		return "Hello World";
+	}
+	
+
+}
